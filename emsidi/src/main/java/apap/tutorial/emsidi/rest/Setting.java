d26ea1d290448bfe1a8be088820811d7af8b75a4
ichsanulamal <muhammad.ichsanul19@gmail.com>
2021-11-10 20:39:38 +0700

    feat: tutorial 5 - done

---
+++ emsidi/src/main/java/apap/tutorial/emsidi/rest/Setting.java
@@ -0,0 +1,6 @@
+package apap.tutorial.emsidi.rest;
+
+public class Setting {
+    final public static String cabangUrl = "https://867c79de-adc9-4a4d-b91c-2bba5fc55504.mock.pstmn.io";
+    final public static String predictUmurUrl = "https://api.agify.io";
+}
